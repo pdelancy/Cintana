736bc90a8ad63ceffaae43221351814f3fb3a2ac
luchen01 <luchenpengnyc@gmail.com>
2018-04-24 13:20:40 -0700

    sync database

--- public/logvisit.js
+++ public/logvisit.js
@@ -1,8 +1,9 @@
 "user strict";
 
-(() => {
+(function() {
 
   function genUUID(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,genUUID)};
+  // var useragent = require('useragent');
 
   let uuidKey = 'horizonsId';
   let uuid = localStorage.getItem(uuidKey);
@@ -21,7 +22,7 @@
   let url = 'http://localhost:3000/api/log/visit/'
      + encodeURIComponent(uuid)
      + '?path='
-     + encodeURIComponent(path);
+     + encodeURIComponent(path)
 
   let img = document.createElement('img');
   img.src = url;
--- src/server/app.js
+++ src/server/app.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const app = express();
+const useragent = require('useragent');
+
 
 const { Pool } = require('pg');
 const db = new Pool({database: 'analytics'});
@@ -10,18 +12,24 @@ app.get("/", () => {
   console.log("HELLO!");
 })
 
-app.get('api/log/visit/:uuid', async (req, res) => {
+app.get('/api/log/visit/:uuid', async (req, res) => {
   try {
+    var agent = useragent.parse(req.headers['user-agent']);
+    agent = agent.toString().split('/');
+
+    let browser = agent[0].split(' ')[0];
+    let os = agent[1].split(' ').slice(0, -1).join(' ');
+
     await db.query(`
       INSERT INTO events
-        (user, timestamp, path, browser, os)
+        (userId, timestamp, path, browser, os)
         VALUES ($1, $2, $3, $4, $5)
     `, [
       req.params.uuid,
       req.query.timestamp || new Date(),
       req.query.path,
-      req.query.browser,
-      req.query.os,
+      browser,
+      os,
     ]);
     res.send('received');
   } catch(e) {
