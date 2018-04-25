a9cdc82cd6e9dbf843ec433cc7ef685db5a00af0
pdelancy <pdelancy94@gmail.com>
2018-04-25 14:52:52 -0700

    eliminate config

--- src/server/app.js
+++ src/server/app.js
@@ -7,13 +7,7 @@ const useragent = require('useragent');
 
 
 const { Pool } = require('pg');
-const db = new Pool({
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  port: process.env.PG_PORT,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-});
+const db = new Pool();
 
 app.get("/api", (req, res) => {
   console.log("HELLO!");
